89ba43be80f18e28fbaac141a16d7fadd7711fab
layla  hammoud <laylahammoud63@gmail.com>
2020-12-29 18:26:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+let routine=[
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+},
+function leashDog(dogName, dogBreed) {
+  console.log(`leash ${dogName} the ${dogBreed}`);
+},
+function walkToPark(dogName, dogBreed) {
+  console.log(`walk to the park with ${dogName} the ${dogBreed}`);
+},
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+},
+function walkHome(dogName, dogBreed) {
+  console.log(`walk home with ${dogName} the ${dogBreed}`);
+},
+function UnleashDog(dogName, dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+},
+]
+function exerciseDog(dogName,dogBreed){
+  console.log(routine)
+}
