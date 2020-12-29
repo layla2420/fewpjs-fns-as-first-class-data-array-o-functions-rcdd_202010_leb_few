520da1c944571b00a42c6f6a7650d8c57b06e846
layla  hammoud <laylahammoud63@gmail.com>
2020-12-29 18:45:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let routine=[
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
-  return`Wake ${dogName} the ${dogBreed}`
+  return `Wake ${dogName} the ${dogBreed}`
 },
 function leashDog(dogName, dogBreed) {
   console.log(`leash ${dogName} the ${dogBreed}`);
-  return`leash ${dogName} the ${dogBreed}`
+  return `leash ${dogName} the ${dogBreed}`
 },
 function walkToPark(dogName, dogBreed) {
   console.log(`walk to the park with ${dogName} the ${dogBreed}`);
@@ -13,7 +13,7 @@ function walkToPark(dogName, dogBreed) {
 },
 function throwFrisbee(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
-    return`Throw the frisbee for ${dogName} the ${dogBreed}`
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
 },
 function walkHome(dogName, dogBreed) {
    console.log(`walk home with ${dogName} the ${dogBreed}`);
@@ -21,15 +21,9 @@ function walkHome(dogName, dogBreed) {
 },
 function UnleashDog(dogName, dogBreed) {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
-    return`Unleash ${dogName} the ${dogBreed}`
+    return `Unleash ${dogName} the ${dogBreed}`
 },
 ]
-function exerciseDog(dogName,dogBreed){
-  let array=[]
-  for (let i=0; i<routine.length; i++) {
-
-  let result=routine[i](dogName,dogBreed);
-  array.push(result)
-  }
-  return array
+function exerciseDog(dog, breed) {
+  return routine.map(fn => fn(dog, breed))
 }
